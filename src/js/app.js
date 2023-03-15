ab6ee07249f34155cc4760836d4e6e87418b2f34
Sarkor2007 <abilkosimovsarkor@gmail.com>
2023-03-15 13:34:21 +0500

    add JS for main page

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import * as functions from "./modules/functions.js";
+import * as header from "./modules/header.js";
 
 functions.isWebp();
+header.headerList();
 
 
---
+++ src/js/modules/header.js
@@ -0,0 +1,25 @@
+export function headerList() {
+    const shareBtn = document.querySelector('.header__share-btn');
+    const shareList = document.querySelector('.header__share-list');
+    const menuBtn = document.querySelector('.header__menu-btn')
+    const menuList = document.querySelector('.header__menu-more')
+    const filter = document.querySelector('.filter');
+
+    shareBtn.addEventListener('click', function () {
+        shareList.classList.toggle('active');
+        filter.classList.toggle('active')
+    })
+
+    menuBtn.addEventListener('click', function () {
+        menuList.classList.toggle('active');
+        filter.classList.toggle('active')
+    })
+
+
+    filter.addEventListener('click', function () {
+        shareList.classList.remove('active')
+        menuList.classList.remove('active')
+        filter.classList.remove('active')
+    })
+
+}
